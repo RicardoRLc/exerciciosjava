1d9ec99e37fe9de1f953f7933228d232658b8d48
RICARDO LUIZ DA COSTA <ricardo.costa@alunos.sc.senac.br>
2019-09-16 21:40:10 -0300

    exercicio1  *2

---
+++ exercicio2.java
@@ -0,0 +1,13 @@
+public class exercicio2 {
+    public static void main(String[] args) {
+                double a = 7;
+                double b = 5;
+                
+                double area = (a * b)  ;
+                
+                    System.out.println(area);     
+               
+                    
+       
+    }
+}               
