4eec9113f848c6aa36d96aee78de924f8ccb90ef
RICARDO LUIZ DA COSTA <ricardo.costa@alunos.sc.senac.br>
2019-10-04 21:58:04 -0300

    avaliacao

--- lista2exercicio7.java
+++ lista2exercicio7.java
@@ -1,13 +1,32 @@
+File arquivo = new File("/home/hallan/nome_do_arquivo.txt");
+try {
+if (!arquivo.exists()) {
+//cria um arquivo (vazio)
+arquivo.createNewFile();
+}
+//caso seja um diretório, é possível listar seus arquivos e diretórios
+File[] arquivos = arquivo.listFiles();
+//escreve no arquivo
+FileWriter fw = new FileWriter(arquivo, true);
+BufferedWriter bw = new BufferedWriter(fw);
+bw.write("Texto a ser escrito no txt");
+bw.newLine();
+bw.close();
+fw.close();
+//faz a leitura do arquivo
 
-import java.util.Scanner;
-
-public class lista2exercicio7 {
-    public static void main(String[] args) {
-        Scanner nota1 = new Scanner(System.in); //linha necessaria para inserir dados 
-        System.out.println("Digite a primeira nota :"); //cout
-        int mes = objeto.nextInt(); //cin       
-
-        
-    }
-    
+FileReader fr = new FileReader(arquivo);
+BufferedReader br = new BufferedReader(fr);
+//enquanto houver mais linhas
+while (br.ready()) {
+//lê a proxima linha
+String linha = br.readLine();
+//faz algo com a linha
+System.out.println(linha);
+}
+br.close();
+fr.close();
+} catch (IOException ex) {
+ex.printStackTrace();
+}
 }
