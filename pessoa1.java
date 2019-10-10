19f02d513d62c60d6242fdfcade4e1b7bdea4664
RICARDO LUIZ DA COSTA <ricardo.costa@alunos.sc.senac.br>
2019-10-09 21:55:58 -0300

    objeto

--- pessoa1.java
+++ pessoa1.java
@@ -1,6 +1,10 @@
 public class Pessoa {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome;
+        pessoa1.peso;
+        pessoa1.altura;
+        pessoa1.dnascimento; 
 
 }
 }    
